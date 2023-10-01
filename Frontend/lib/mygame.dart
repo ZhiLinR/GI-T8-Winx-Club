c8862e1e9483a1d51bec0108255f122ebbaa1546
Sharon Chua <115637619+scyt01@users.noreply.github.com>
2023-10-01 14:31:56 +0800

    Map rendering
    - edit homemap.tmx, mygame.dart, pubspec.yaml

--- Frontend/lib/mygame.dart
+++ Frontend/lib/mygame.dart
@@ -1,7 +1,9 @@
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 import 'components/player.dart';
 import 'helpers/direction.dart';
-import 'package:flame_tiled/flame_tiled.dart';
 
 class MyGame extends FlameGame {
   final Player _player = Player();
@@ -10,6 +12,28 @@ class MyGame extends FlameGame {
   Future<void> onLoad() async {
     final homeMap = await TiledComponent.load('homemap.tmx', Vector2.all(32));
     add(homeMap);
+
+    final objectGroup = homeMap.tileMap.getLayer<ObjectGroup>('AnimatedSink');
+    final sink = await Flame.images.load('sink.png');
+
+    for (final object in objectGroup!.objects) {
+      world.add(
+        SpriteAnimationComponent(
+          size: Vector2.all(32.0),
+          position: Vector2(object.x, object.y),
+          animation: SpriteAnimation.fromFrameData(
+            sink,
+            SpriteAnimationData.sequenced(
+              amount: 3,
+              stepTime: .15,
+              textureSize: Vector2.all(32),
+            ),
+          ),
+        ),
+      );
+    }
+
+    // PLAYER
     add(_player);
   }
 

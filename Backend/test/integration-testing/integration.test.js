63924ca62d009870b938c5f980396226689db3c6
YUN WADDY SOE _ <yunsoe3105@gmail.com>
2023-10-03 04:05:07 +0800

    backend integration testing

--- Backend/test/integration-testing/integration.test.js
+++ Backend/test/integration-testing/integration.test.js
@@ -1,2 +1,48 @@
 const db = require("../../app/models");
 const Controller = require("../../app/controllers/controller");
+
+var accounts = [
+    {
+        "username": "sophie",
+        "password": "12345",
+        "nickname": "sophie",
+    },
+    {
+        "username": "yun",
+        "password": "12345",
+        "nickname": "yun",
+    },
+    {
+        "username": "ethan",
+        "password": "12345",
+        "nickname": "ethan",
+    }
+];
+
+describe("Account API", () => {
+    beforeAll(async () => {
+        await db.mongoose
+            .set("strictQuery", true)
+            .connect(db.url, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
+            .catch((err) => {
+                console.log("Cannot connect to the database!", err);
+                process.exit();
+            });
+
+        await db.accounts.insertMany(accounts);
+    });
+
+    // run before each test
+    afterAll(async () => {
+        await db.accounts.deleteMany({});
+        await db.mongoose.disconnect();
+    });
+
+    it("should get account by username", async () => {
+        const result = await Controller.findAccountByUsername("/accounts/:username");
+        expect(result).toMatchObject(accounts[0]);
+    })
+})
